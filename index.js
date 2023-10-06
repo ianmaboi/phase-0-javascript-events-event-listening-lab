72b1e86e047497a7cdcd03a1eac1121c0550cb32
ianmaboimonene <ian.maboi@student.moringaschool.com>
2023-10-06 09:34:33 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-function addingEventListener() {
+//function clickAlert(){
+    //alert("I was clicked");
+// }
+
+function addingEventListener(butt) {
+    const element =document.getElementById("input");
+    if("button"){
+        element.addEventListener("click",function(){
+            alert("Button clicked!");
+        })
+    }
 }
+// document.addEventListener("DOMContentLoaded", function{
+// const input =document.getElementById("input");
+
+//  input.addEventListener("click", function(){
+//   alert("I was clicked!");
+// });
+// return true;
+// })
